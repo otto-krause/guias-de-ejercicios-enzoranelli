25ea81f5f85b191954e2d0c757c1831de2a207af
enzoranelli <enzo.ranelli@gmail.com>
2020-04-22 19:03:55 -0300

    se  ingresa los numeros mediante un while.Lluego se muestra en un for,la cantidad  de numeros,el 1°num de la lista y el ultimo y numeros mayores a 50 mediante un for e if

--- Guia 2/E1/Program.cs
+++ Guia 2/E1/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace E1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numeros = new List<int>();
+            int num=1;
+            while(num>0)
+            {
+                Console.WriteLine("Ingrese una lista de numeros distinta a cero:");
+                num=Int32.Parse(Console.ReadLine());
+                if(num>0)
+                {
+                    numeros.Add(num);
+                }
+            }
+            Console.WriteLine("Los numeros de la lista son:");
+            for (int i=0;i<numeros.Count;i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            Console.WriteLine("\nLa cantidad de numeros que tiene la lista es: "+numeros.Count);
+            Console.WriteLine("El primer numero de la lista es "+numeros[0]+" y el ultimo es "+numeros[numeros.Count-1]);
+            Console.WriteLine("\nLos numeros mayores a 50 de esta lista son: ");
+            for (int i = 0; i<numeros.Count;i++)
+            {
+                if(numeros[i]>50)
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+            }
+
         }
     }
 }
